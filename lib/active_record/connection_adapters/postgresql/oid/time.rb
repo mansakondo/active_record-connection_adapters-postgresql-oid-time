85970c97e5f091fc8fd891b2fb0445af2fd18d55
mansakondo <mansakondo22@gmail.com>
2022-08-25 14:26:12 +0200

    Create and register the `pg_time` type

--- lib/active_record/connection_adapters/postgresql/oid/time.rb
+++ lib/active_record/connection_adapters/postgresql/oid/time.rb
@@ -2,13 +2,68 @@ require "active_record/connection_adapters/postgresql/oid/time/version"
 require "active_record/connection_adapters/postgresql/oid/time/railtie"
 
 module ActiveRecord
+  require "active_record/connection_adapters"
+
   module ConnectionAdapters
-    module Postgresql
-      module Oid
-        module Time
-          # Your code goes here...
+    module PostgreSQL
+      module OID
+        class Time < Type::Value
+          class Value < Type::Time::Value
+            def initialize(ruby_time, postgres_time)
+              super(ruby_time)
+              @postgres_time = postgres_time
+            end
+
+            def change(time:, **args)
+              @postgres_time = time
+              self.class.new(super(**args), time)
+            end
+
+            def in_time_zone
+              self.class.new(super, @postgres_time)
+            end
+
+            def to_time
+              self.class.new(super, @postgres_time)
+            end
+
+            def inspect
+              to_s
+            end
+
+            def as_json
+              to_s
+            end
+
+            def to_json
+              as_json.to_json
+            end
+
+            def to_s
+              @postgres_time
+            end
+          end
+
+          def serialize(value)
+            value.to_s
+          end
+
+          private
+            def cast_value(value)
+              case value
+              when String
+                Value.new(::Time.parse(value), value)
+              when Value
+                value
+              else
+                return
+              end
+            end
         end
       end
     end
+
+    require "active_record/connection_adapters/postgresql_adapter"
+    require "active_record/connection_adapters/postgresql/register_time_type"
   end
 end
---
+++ lib/active_record/connection_adapters/postgresql/register_time_type.rb
@@ -0,0 +1,22 @@
+module ActiveRecord
+  module ConnectionAdapters
+    class PostgreSQLAdapter < AbstractAdapter
+      module RegisterTimeType
+        extend ActiveSupport::Concern
+
+        prepended do
+          ActiveRecord::Type.register(:pg_time, OID::Time, adapter: :postgresql)
+        end
+
+        class_methods do
+          def initialize_type_map(m)
+            super
+            register_class_with_precision m, "pg_time", OID::Time
+          end
+        end
+      end
+
+      prepend RegisterTimeType
+    end
+  end
+end
